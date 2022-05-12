924ad6277788eec1b5d3dfb8841cd4d2f5e2c5bf
Casper1434 <casper@ck-hosting.com>
2022-05-12 19:48:33 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,110 @@
+// #   ufw.disable()        # disable firewall
+// #   ufw.enable()         # enable firewall
+// #   ufw.allow()          # default allow -- allow all
+// #   ufw.allow(22)        # allow port 22, any protocol
+// #   ufw.allow(22,'tcp')  # allow port 22, tcp protocol
+// #   ufw.allow('22,'tcp')  # allow port 22, tcp protocol
+// #   ufw.allow(53,'udp')  # allow port 53, udp protocol
+// #   ufw.allow(53,'udp')  # allow port 53, udp protocol
+// #   ufw.deny()           # default deny -- deny all
+// #   ufw.deny(22,'tcp')   # deny port 22, tcp protocol
+// #   ufw.delete(22)       # delete rules referencing port 22 !!!!!!!!!!!!!!!!!!!!!!
+// #   ufw.reset()          # restore defaults
+// #   ufw.status()         # return status string
+// #   ufw.run("allow 22")  # directly run command as if from command line
+
+
+
+const express= require('express')
+const app = express()
+
+const config = require('./config.json')
+
+const ufw = require("./ufw");
+
+const bodyparser = require('body-parser')
+app.use(bodyparser.text())
+
+app.get('/api/run/:command',(req,res)=>{ 
+   var out = ufw.run(req.params.command)
+   res.json({ output: `${out}` })
+})
+
+app.get('/api/disable', (req,res)=>{
+    var out = ufw.disable()
+    res.json({ output: `${out}`})
+})
+
+app.get('/api/enable', (req,res)=>{
+    var out = ufw.enable()
+    res.json({ output: `${out}`})
+})
+
+app.get('/api/allow', (req,res)=>{
+    var out = ufw.allow()
+    res.json({ output: `${out}`})
+})
+
+app.get('/api/allow/:port', (req,res)=>{
+    var out = ufw.allow(req.params.port)
+    res.json({ output: `${out}`})
+})
+
+app.get('/api/allow/:port/:protocol', (req,res)=>{
+    var out = ufw.allow(req.params.port, req.params.protocol)
+    res.json({ output: `${out}`})
+})
+
+app.get('/api/deny', (req,res)=>{
+    var out = ufw.deny()
+    res.json({ output: `${out}`})
+})
+
+app.get('/api/deny/:port', (req,res)=>{
+    var out = ufw.deny(req.params.port)
+    res.json({ output: `${out}`})
+})
+
+app.get('/api/deny/:port/:protocol', (req,res)=>{
+    var out = ufw.deny(req.params.port, req.params.protocol)
+    res.json({ output: `${out}`})
+})
+
+app.get('/api/reset', (req,res)=>{
+    var out = ufw.reset()
+    res.json({ output: `${out}`})
+})
+
+app.get('/api/status', (req,res)=>{
+    var out = ufw.status()
+    res.json({ output: `${out}`})
+})
+
+app.get('/api/deny_ip/:ip', (req,res)=>{
+    var out = ufw.deny_ip(req.params.ip)
+    res.json({ output: `${out}`})
+})
+
+app.get('/api/deny_ip/ip:/:port', (req,res)=>{
+    var out = ufw.deny_ip(req.params.ip, req.params.port)
+    res.json({ output: `${out}`})
+})
+
+app.get('/api/deny_ip/ip:/:port/:protocol', (req,res)=>{
+    var out = ufw.deny_ip(req.params.ip, req.params.port, req.params.protocol)
+    res.json({ output: `${out}`})
+})
+
+app.get('/api/show_rules', (req,res)=>{
+    var out = ufw.show_rules()
+    res.send(`${out}`)
+})
+
+
+app.get('/api/remove/:port', (req,res)=>{
+    var out = ufw.remove(req.params.port)
+    res.send(`${out}`)
+})
+
+
+app.listen(config.port)
---
+++ ufw.js
@@ -0,0 +1,86 @@
+// directly run command as if from command line
+function run(actionstr) {
+    const execSync = require('child_process').execSync;
+    // import { execSync } from 'child_process';  // replace ^ if using ES modules
+
+    const output = execSync(`sudo ufw ${actionstr}`, { encoding: 'utf-8' });  // the default is 'buffer'
+    return output
+}
+
+// disable firewall
+function disable() {
+    return run('--force disable')
+}
+
+// enable firewall
+function enable() {
+    return run('--force enable')
+}
+
+// default allow -- allow all
+function allow() {
+    return run('default allow outgoing')
+}  
+
+// allow port, any protocol
+function allow(port) {
+    return run(`allow ${port}`)
+} 
+
+// allow port, tcp or udp protocol
+function allow(port, protocol) {
+    return run(`allow ${port}/${protocol}`)
+}
+
+//default deny -- deny all
+function deny() {
+    return run('default deny incoming')
+}
+
+// deny port, any protocol
+function deny(port) {
+    return run(`deny ${port}`)
+} 
+
+// deny port, tcp or udp protocol
+function deny(port, protocol) {
+    return run(`deny ${port}/${protocol}`)
+}
+
+
+function show_rules() {
+   return run(`status`).replaceAll('\n', '<br>').replaceAll('\r', '')
+}
+
+// delete rules referencing port
+function remove(rule) {
+    return run(`delete ${rule}`)
+}
+
+// restore defaults
+function reset() {
+    return run('--force reset')
+}
+
+// return status string
+function status() {
+    return run('status verbose')
+} 
+
+// deny specific ip
+function deny_ip(ip) {
+    return run(`deny from ${ip}`)
+}
+
+// deny specific ip and port
+function deny_ip(ip, port) {
+    return run(`deny from ${ip} to any ${port}`)
+}
+
+// deny specific ip and port, protocol
+function deny_ip(ip, port, protocol) {
+    return run(`deny from ${ip} to ${protocol} ${port}`)
+}
+
+
+module.exports = { run, disable, enable, allow, deny, remove, reset, status, deny_ip, show_rules };
